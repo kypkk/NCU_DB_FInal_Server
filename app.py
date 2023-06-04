6043936f72c497a99fa60a8c3be418a0b8ffa513
kypkk <kypss50102@gmail.com>
2023-06-05 01:19:02 +0800

    feat: transaction, initial account, get_stock strategy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import io
 import base64
 from PIL import Image
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
 from flask import Response
 import pandas as pd
 import os
@@ -21,6 +21,27 @@ db_settings = {
     "charset": "utf8"
 }
 
+def is_transaction_legal(date, buy_or_sell, stock_code, shares, price):
+    try:
+        date = '\'' + date +'\''
+        conn = pymssql.connect(**db_settings)
+        with conn.cursor() as cursor:
+                command = f"select TOP(1) remain_cash as remain_cash, (select sum(buy_or_sell * shares) from stock_transactions where stock_code={stock_code}) as shares from stock_transactions order by id DESC"
+                cursor.execute(command)
+                remain_cash, own_shares = cursor.fetchall()[0]
+                if buy_or_sell == 1:
+                    if remain_cash >= price * shares:
+                        return True
+                elif buy_or_sell == -1:
+                    if own_shares >= shares:
+                        return True
+                else:
+                    raise Exception("buy_or_sell not 1 or -1")
+                return "buy_or_sell not 1 or -1"
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        raise
 
 def get_buy_or_sell(buy_or_sell, close):
     buy_points = []
@@ -38,6 +59,8 @@ def get_buy_or_sell(buy_or_sell, close):
     return buy_points, sell_points
 
 
+conn = pymssql.connect(**db_settings)
+b = io.BytesIO()
 app = Flask(__name__)
 
 
@@ -51,49 +74,78 @@ def hello():
     return "hello world"
 
 
-@app.route('/plot-candle')
-def plot_candle():
+@app.route('/get_stock', methods=['POST'])
+def get_date():
+    data = request.json
     try:
-        b = io.BytesIO()
-        conn = pymssql.connect(**db_settings)
-        # get stock data
         with conn.cursor() as cursor:
-            command = "SELECT dbo.股價資訊.date, o, h, l, c, v, Buy_sell FROM [dbo].[股價資訊] left JOIN (SELECT * from dbo.GB_rule5('2330')) as t1 on dbo.股價資訊.date = t1.date where stock_code = '2330'"
+            date = '\'' + data["date"] +'\''
+            command = f"select * from datefind_stockcode({date})"
             cursor.execute(command)
             result = cursor.fetchall()
             result = np.array(result)
 
-            # define column names
-            column_names = ['Open', 'High', 'Low',
-                            'Close', 'Volume', 'buy_or_sell']
-
-            # get data and set index
-            result_df = pd.DataFrame(
-                result[:, 1:], columns=column_names, index=result[:, 0], dtype=float)
+            column_names = ['Company', 'Buy_or_sell']
+            result_df = pd.DataFrame(result[:,1:], columns=column_names, index=result[:,0], dtype=float)
             result_df.index.name = 'Date'
             result_df.index = pd.to_datetime(result_df.index)
-            # print(result_df)
+            result_df = result_df.to_json(orient="columns")
 
-            # get buy and sell
-            buy_arr, sell_arr = get_buy_or_sell(
-                result_df['buy_or_sell'], result_df['Close'])
-
-            apdict = [mpf.make_addplot(sell_arr, type='scatter', markersize=200, marker='^'),
-                      mpf.make_addplot(buy_arr, type='scatter',
-                                       markersize=200, marker='v'),
-                      ]
-            # print(len(result_df.index))
-
-            mpf.plot(result_df, type='candle', style='yahoo',
-                     savefig=b)
-
+            print(result_df)
+            return result_df
         conn.commit()
-        conn.close()
-    except Exception as e:
-        return e
-    encoded_img_data = base64.b64encode(b.getvalue())
 
-    return {'img_data': encoded_img_data.decode('utf-8')}
+    except Exception as e:
+        print(e)
+        raise
+
+@app.route('/initial_account', methods=['POST'])
+def initialize_account():
+    data = request.json
+    try:
+        date = '\'' + data['date'] +'\''
+        deposit = int(data['deposit'])
+        with conn.cursor() as cursor:
+            command = f"insert into stock_transactions(id, date, buy_or_sell ,remain_cash) values(0, {date}, 0,{deposit})"
+            cursor.execute(command)
+            print("init sucessful transaction");
+        conn.commit()
+        return "Init sucessful account"
+    except Exception as e:
+        print(e)
+        raise
+
+@app.route('/recordTransaction', methods=['POST'])
+def record_transaction():
+    data = request.json
+    date = data['date'] 
+    buy_or_sell = data['buy_or_sell']
+    stock_code = data['stock_code']
+    shares = data['shares']
+    price = data['price']
+    print(stock_code)
+    stock_code = '\''+stock_code+'\''
+    try:
+        if not is_transaction_legal(date, buy_or_sell, stock_code, shares, price):
+            raise Exception("No enough cash or shares")
+        with conn.cursor() as cursor:
+            date = '\'' + date +'\''
+            command = f"INSERT INTO stock_transactions (id, date, stock_code, stock_price, shares, buy_or_sell, remain_cash)\
+                        SELECT MAX(id) + 1, {date}, {stock_code},\
+                               {price},\
+                               {shares},\
+                               {buy_or_sell},\
+                               ((SELECT TOP(1)remain_cash FROM stock_transactions ORDER BY id DESC) +\
+                                {buy_or_sell} * -1 *{price})\
+                        FROM stock_transactions;"
+            cursor.execute(command)
+        conn.commit()
+        return "Transaction successfully created"
+        
+    except Exception as e:
+        print(e)
+        raise
+
 
 
 if __name__ == '__main__':
